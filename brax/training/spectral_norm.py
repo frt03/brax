409f1ff7de4f4ac46b5e5b4c4b0fef0694302f22
furuta <furuta0313@gmail.com>
2021-08-23 17:13:00 +0900

    fix spectral_norm

--- brax/training/spectral_norm.py
+++ brax/training/spectral_norm.py
@@ -69,18 +69,20 @@ class SpectralNorm(linen.Module):
         w = jnp.reshape(w, [-1, w.shape[-1]])
 
     key = self.make_rng('sing_vec')
-    u0_state = self.variable('sing_vec', 'u0', initializers.normal(stddev=1.), key, (1, w.shape[-1]))
-    u0 = u0_state.value
+    u0 = self.variable('sing_vec', 'u0', initializers.normal(stddev=1.), key, (1, w.shape[-1]))
+    # u0 = u0_state.value
 
     # Power iteration for the weight's singular value.
     for _ in range(self.n_steps):
-      v0 = _l2_normalize(jnp.matmul(u0, w.transpose([1, 0])), eps=self.eps)
-      u0 = _l2_normalize(jnp.matmul(v0, w), eps=self.eps)
+      # v0 = _l2_normalize(jnp.matmul(u0, w.transpose([1, 0])), eps=self.eps)
+      # u0 = _l2_normalize(jnp.matmul(v0, w), eps=self.eps)
+      v0 = _l2_normalize(jnp.matmul(u0.value, w.transpose([1, 0])), eps=self.eps)
+      u0.value = _l2_normalize(jnp.matmul(v0, w), eps=self.eps)
 
-    # u0 = lax.stop_gradient(u0)
-    # v0 = lax.stop_gradient(v0)
+    u0.value = lax.stop_gradient(u0.value)
+    v0 = lax.stop_gradient(v0)
 
-    sigma = jnp.matmul(jnp.matmul(v0, w), jnp.transpose(u0))[0, 0]
+    sigma = jnp.matmul(jnp.matmul(v0, w), jnp.transpose(u0.value))[0, 0]
 
     w /= sigma
     w_bar = w.reshape(w_shape)
