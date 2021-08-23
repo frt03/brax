2071ac7845b0cc8952075a1d1184cbd1d2ca4f7f
furuta <furuta0313@gmail.com>
2021-08-23 18:59:52 +0900

    cleanup code

--- brax/training/networks.py
+++ brax/training/networks.py
@@ -19,11 +19,10 @@ from typing import Any, Callable, Sequence, Tuple
 
 import dataclasses
 from flax import linen
-from flax.core import freeze, unfreeze
 import jax
 import jax.numpy as jnp
 
-from brax.training.spectral_norm import SpectralNorm, SNDense
+from brax.training.spectral_norm import SNDense
 
 @dataclasses.dataclass
 class FeedForwardModel:
@@ -53,8 +52,8 @@ class MLP(linen.Module):
         hidden = self.activation(hidden)
     return hidden
 
-class MLP2(linen.Module):
-  """MLP module."""
+class SNMLP(linen.Module):
+  """MLP module with Spectral Normalization."""
   layer_sizes: Sequence[int]
   activation: Callable[[jnp.ndarray], jnp.ndarray] = linen.relu
   kernel_init: Callable[..., Any] = jax.nn.initializers.lecun_uniform()
@@ -75,60 +74,6 @@ class MLP2(linen.Module):
         hidden = self.activation(hidden)
     return hidden
 
-class SNMLP:
-  def __init__(self, mlp: Any):
-    # wrapper for spectral normalization
-    self.mlp = mlp
-    self.layer_sizes: Sequence[int] = mlp.layer_sizes
-    #self.spectral_normalization = [
-    #  SpectralNorm(dtype=jnp.float32, eps=1e-4, n_steps=1) for _ in range(len(self.layer_sizes))
-    #]
-    self.idx = 0
-
-  def init(self, rng, dummy_x):
-    rngs = jax.random.split(rng, len(self.layer_sizes)*2 + 1)
-    params = self.mlp.init(rngs[0], dummy_x)
-    sn_params = []
-    unfreezed_params = unfreeze(params)['params']
-    self.idx = 0
-    for _, param_dict in unfreezed_params.items():
-      for k, v in param_dict.items():
-        if k == 'kernel':
-          sn_params.append(
-            SpectralNorm(dtype=jnp.float32, eps=1e-4, n_steps=1).init(
-              {'params': rngs[self.idx+1], 'sing_vec': rngs[self.idx+len(self.layer_sizes)]},
-              v
-            )
-          )
-          self.idx += 1
-    self.idx = 0
-    self.sn_params = sn_params
-    return params
-
-  def apply(self, params, x, rng):
-    rngs = jax.random.split(rng, len(self.layer_sizes) + 1)
-    new_params = {}
-    unfreezed_params = unfreeze(params)['params']
-    self.idx = 0
-    for module_name, param_dict in unfreezed_params.items():
-      new_params[module_name] = {}
-      for k, v in param_dict.items():
-        def apply_sn_to_kernel(k, v):
-          if k == 'kernel':
-            applied_v, self.sn_params[self.idx] = SpectralNorm(dtype=jnp.float32, eps=1e-4, n_steps=1).apply(
-              self.sn_params[self.idx], v,
-              rngs={'sing_vec': rngs[self.idx+1]}, mutable=['sing_vec']
-            )
-            self.idx += 1
-            return applied_v
-          elif k == 'bias':
-            return v
-        new_params[module_name][k] = apply_sn_to_kernel(k, v)
-    params = freeze({'params': new_params})
-    self.idx = 0
-    y = self.mlp.apply(params, x)
-    return y
-
 def make_model(layer_sizes: Sequence[int],
                obs_size: int,
                activation: Callable[[jnp.ndarray], jnp.ndarray] = linen.swish,
@@ -144,9 +89,8 @@ def make_model(layer_sizes: Sequence[int],
   Returns:
     a model
   """
-  mlp = MLP2 if spectral_norm else MLP
+  mlp = SNMLP if spectral_norm else MLP
   module = mlp(layer_sizes=layer_sizes, activation=activation)
-  # module = SNMLP(mlp=mlp) if spectral_norm else mlp
   dummy_obs = jnp.zeros((1, obs_size))
   if spectral_norm:
     return FeedForwardModel(
--- brax/training/spectral_norm.py
+++ brax/training/spectral_norm.py
@@ -1,14 +1,11 @@
 # from dm-haiku: https://github.com/deepmind/dm-haiku/blob/main/haiku/_src/spectral_norm.py
-"""Flax-style Normalization module."""
-
-from typing import (Any, Callable, Optional, Tuple)
+from typing import (Any, Callable, Tuple)
 
 from jax import lax
-from jax.nn import initializers
 import jax.numpy as jnp
 
 from flax import linen
-from flax.linen.initializers import lecun_normal, zeros
+from flax.linen.initializers import lecun_normal, normal, zeros
 
 
 default_kernel_init = lecun_normal()
@@ -36,70 +33,9 @@ def _l2_normalize(x, axis=None, eps=1e-12):
   return x * lax.rsqrt((x * x).sum(axis=axis, keepdims=True) + eps)
 
 
-class SpectralNorm(linen.Module):
-  """Normalizes an input by its first singular value.
-  This module uses power iteration to calculate this value based on the
-  input and an internal hidden state.
-  """
-  dtype: Any = jnp.float32
-  eps: float = 1e-4
-  n_steps: int = 1
-
-  @linen.compact
-  def __call__(self, w) -> jnp.ndarray:
-    """Applies layer normalization on the input.
-    Args:
-      w: the inputs
-    Returns:
-      Normalized inputs (the same shape as inputs).
-    """
-    w = jnp.asarray(w, jnp.float32)
-
-    update_stats = True
-    error_on_non_matrix = False
-    w_shape = w.shape
-
-    # Handle scalars.
-    if w.ndim <= 1:
-      raise ValueError("Spectral normalization is not well defined for "
-                      "scalar inputs.")
-    # Handle higher-order tensors.
-    elif w.ndim > 2:
-      if error_on_non_matrix:
-        raise ValueError(
-            f"Input is {w.ndim}D but error_on_non_matrix is True")
-      else:
-        w = jnp.reshape(w, [-1, w.shape[-1]])
-
-    key = self.make_rng('sing_vec')
-    u0 = self.variable('sing_vec', 'u0', initializers.normal(stddev=1.), key, (1, w.shape[-1]))
-    # u0_state = self.variable('sing_vec', 'u0', initializers.normal(stddev=1.), key, (1, w.shape[-1]))
-    # u0 = u0_state.value
-
-    # Power iteration for the weight's singular value.
-    for _ in range(self.n_steps):
-      # v0 = _l2_normalize(jnp.matmul(u0, w.transpose([1, 0])), eps=self.eps)
-      # u0 = _l2_normalize(jnp.matmul(v0, w), eps=self.eps)
-      v0 = _l2_normalize(jnp.matmul(u0.value, w.transpose([1, 0])), eps=self.eps)
-      u0.value = _l2_normalize(jnp.matmul(v0, w), eps=self.eps)
-
-    u0.value = lax.stop_gradient(u0.value)
-    v0 = lax.stop_gradient(v0)
-
-    sigma = jnp.matmul(jnp.matmul(v0, w), jnp.transpose(u0.value))[0, 0]
-
-    w /= sigma
-    w_bar = w.reshape(w_shape)
-
-    # if update_stats:
-      # u0_state.value = u0
-
-    return w_bar.astype(self.dtype)
-
-
 class SNDense(linen.Module):
   """A linear transformation applied over the last dimension of the input
-  with spectral normalization.
+  with spectral normalization (https://arxiv.org/abs/1802.05957).
 
   Attributes:
     features: the number of output features.
@@ -135,7 +71,7 @@ class SNDense(linen.Module):
                         (inputs.shape[-1], self.features))
     kernel = jnp.asarray(kernel, self.dtype)
 
-    # for spectral normalization
+    """for spectral normalization"""
     kernel_shape = kernel.shape
     # Handle scalars.
     if kernel.ndim <= 1:
@@ -145,7 +81,7 @@ class SNDense(linen.Module):
     elif kernel.ndim > 2:
       kernel = jnp.reshape(kernel, [-1, kernel.shape[-1]])
     key = self.make_rng('sing_vec')
-    u0_state = self.variable('sing_vec', 'u0', initializers.normal(stddev=1.), key, (1, kernel.shape[-1]))
+    u0_state = self.variable('sing_vec', 'u0', normal(stddev=1.), key, (1, kernel.shape[-1]))
     u0 = u0_state.value
 
     # Power iteration for the weight's singular value.
