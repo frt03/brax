6ace96217d5e25539542c1a1a5255c6ba549af59
furuta <furuta0313@gmail.com>
2021-12-06 14:01:54 +0900

    for debug

--- brax/experimental/composer/envs/ma_descs.py
+++ brax/experimental/composer/envs/ma_descs.py
@@ -286,11 +286,11 @@ def add_squidgame(env_desc: Dict[str, Any],
 def add_robosumo(
     env_desc: Dict[str, Any],
     ring_size: float = 3.0,
-    draw_scale: float = 1.,
+    draw_scale: float = 0.,
     knocking_scale: float = 1.,
     control_scale: float = 1.,
     opp_scale: float = 1.,
-    win_bonus: float = 2000.,
+    win_bonus: float = 0.,
 ):
   """Add a sumo task."""
   agents = sorted(env_desc['components'])
