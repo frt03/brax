7f6f709fb6c4c94b47b1fb5d5a38c0e38d43ca80
frt03 <furuta0313@gmail.com>
2021-12-10 15:02:39 +0900

    fix centipade

--- brax/experimental/composer/components/centipede.py
+++ brax/experimental/composer/components/centipede.py
@@ -170,29 +170,29 @@ def generate_centipade_config_with_n_torso(n):
 
     return tmp, collides
 
-    def template_joint(ind):
-      tmp = f"""
-        joints {{
-          name: "torso_{str(ind)}_torso_{str(ind+1)}"
-          parent_offset {{ x: 0.25  }}
-          child_offset {{ x: -0.25  }}
-          parent: "torso_{str(ind)}"
-          child: "torso_{str(ind+1)}"
-          stiffness: 5000.0
-          angular_damping: 35
-          angle_limit {{ min: -20.0 max: 20.0 }}
-          rotation {{ y: -90 }}
-          reference_rotation {{ y: -0.0 }}
-        }}
-        actuators {{
-          name: "torso_{str(ind)}_torso_{str(ind+1)}"
-          joint: "torso_{str(ind)}_torso_{str(ind+1)}"
-          strength: 300.0
-          torque {{}}
-        }}
-        """
-      collides = tuple()
-      return tmp, collides
+  def template_joint(ind):
+    tmp = f"""
+      joints {{
+        name: "torso_{str(ind)}_torso_{str(ind+1)}"
+        parent_offset {{ x: 0.25  }}
+        child_offset {{ x: -0.25  }}
+        parent: "torso_{str(ind)}"
+        child: "torso_{str(ind+1)}"
+        stiffness: 5000.0
+        angular_damping: 35
+        angle_limit {{ min: -20.0 max: 20.0 }}
+        rotation {{ y: -90 }}
+        reference_rotation {{ y: -0.0 }}
+      }}
+      actuators {{
+        name: "torso_{str(ind)}_torso_{str(ind+1)}"
+        joint: "torso_{str(ind)}_torso_{str(ind+1)}"
+        strength: 300.0
+        torque {{}}
+      }}
+      """
+    collides = tuple()
+    return tmp, collides
 
   base_config = f""""""
   collides = tuple()
