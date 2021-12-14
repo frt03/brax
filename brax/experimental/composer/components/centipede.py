015147f776d84e1fb06a95edc511410edefc63a6
frt03 <furuta0313@gmail.com>
2021-12-15 04:07:59 +0900

    fix config

--- brax/experimental/composer/components/centipede.py
+++ brax/experimental/composer/components/centipede.py
@@ -183,7 +183,7 @@ def generate_centipede_config_with_n_torso(n):
         stiffness: 5000.0
         angular_damping: 35
         angle_limit {{ min: -20.0 max: 20.0 }}
-        rotation {{ y: -90 z: 60.0 }}
+        rotation {{ y: -90 z: 0.0 }}
         reference_rotation {{ y: 0.0 }}
       }}
       actuators {{
